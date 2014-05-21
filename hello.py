fe92ac8d4bfa9c3ff6675d059b637847a5ed145e
Drew Wimmer <drewwimmer68@gmail.com>
2014-05-20 19:30:29 -0500

    Script commit

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 print("Hello World")
 print("checking commits")
 print("Dev Branch")
+
+#!/usr/bin/python 
+import os 
+import urllib 
+google = os.popen('zenity --entry --text="Enter what you want to google: " --title="google.py"').read() 
+google = urllib.quote(google) 
+os.system('firefox http://www.google.com/search?q=%s' % (google))  
